4d96de93a1d6e4dd4c58a05a45bd4f77260c08e9
DESKTOP-3HCTKM7\Anh Tuan <anhtuan01041998@gmail.com>
2020-12-31 14:24:46 +0700

    add func print result

--- ex1.go
+++ ex1.go
@@ -1,7 +1,5 @@
 package main
 
-//C:/Users/Anh Tuan/Desktop/config.json
-
 import (
 	"bufio"
 	"encoding/json"
@@ -24,54 +22,63 @@ type Product struct {
 func isJsonFile(fileName string) bool {
 	temp := fileName[len(fileName)-7:]
 	temp = strings.TrimRight(temp, "\r\n")
-
 	if strings.Compare(temp, ".json") == 0 {
 		return true
 	} else {
 		return false
 	}
 }
+
 func isExit(fileName string) bool {
 	temp := fileName[len(fileName)-5:]
-
 	if strings.Compare(temp, "Exit") == 0 {
 		return true
 	} else {
 		return false
 	}
 }
-func readData(fileName string) *Instance {
 
+func readData(fileName string) *Instance {
 	fileName = strings.TrimRight(fileName, "\r\n")
 	jsonFile, err := os.Open(fileName)
 	if err != nil {
 		//fmt.Println("Error!")
 		fmt.Println(err)
 	}
-
 	defer jsonFile.Close()
-
-	//products := make([]Product, 3)
+	//instance := make([]Instance, 3)
 	var instance Instance
 	raw, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-
 	json.Unmarshal(raw, &instance)
 	return &instance
 }
-func printProduct(instance *Instance) {
 
+func printProduct(instance *Instance) {
 	for i := 0; i < len(instance.Instance); i++ {
-		fmt.Print("Type: ", instance.Instance[i].Type)
-		fmt.Print(" vCPU: ", instance.Instance[i].VCPU)
-		fmt.Print(" vRam: ", instance.Instance[i].VRam)
-		fmt.Print(" counts: ", instance.Instance[i].Counts)
-		fmt.Print("\n")
+		fmt.Printf("Type: %-10v", instance.Instance[i].Type)
+		fmt.Printf(" vCPU: %-10v", instance.Instance[i].VCPU)
+		fmt.Printf(" vRam: %-10v", instance.Instance[i].VRam)
+		fmt.Printf(" counts: %-10v", instance.Instance[i].Counts)
+		fmt.Printf("\n")
 	}
 	fmt.Print("\n")
+}
+
+func printResult(oldInstance, newInstance *Instance) {
+	for i := 0; i < len(newInstance.Instance); i++ {
+		temp := newInstance.Instance[i].Counts - oldInstance.Instance[i].Counts
+		if temp >= 0 {
+			//fmt.Println("[\""+oldInstance.Instance[i].Type+"\"]"+" [provision] [", temp, "]")
+			fmt.Printf("[\"%-9v\"]	[%-9v] [%-1v]\n", oldInstance.Instance[i].Type, "provision", temp)
+		} else {
+			//fmt.Println("[\""+oldInstance.Instance[i].Type+"\"]"+" [delete] [", -temp, "]")
+			fmt.Printf("[\"%-9v\"]	[%-9v] [%-1v]\n", oldInstance.Instance[i].Type, "delete", -temp)
+		}
+	}
 
 }
 
@@ -86,31 +93,18 @@ func main() {
 	for exit := 1; exit != 2; {
 		fmt.Print("Enter path: ")
 		path, _ := reader.ReadString('\n')
-
 		if isExit(path) {
 			exit = 2
 			break
 		} else {
 			if isJsonFile(path) {
-
 				//readData(path)
 				if check {
 					oldInstance = newInstance
 					newInstance = readData(path)
-
 					printProduct(oldInstance)
 					printProduct(newInstance)
-
-					for i := 0; i < len(newInstance.Instance); i++ {
-						temp := newInstance.Instance[i].Counts - oldInstance.Instance[i].Counts
-						if temp >= 0 {
-							fmt.Println("[\""+oldInstance.Instance[i].Type+"\"]"+" [provision] [", temp, "]")
-						} else {
-							fmt.Println("["+oldInstance.Instance[i].Type+"]"+" [delete] [", -temp, "]")
-						}
-
-					}
-
+					printResult(oldInstance, newInstance)
 				} else {
 					newInstance = readData(path)
 					printProduct(newInstance)
@@ -120,7 +114,6 @@ func main() {
 			} else {
 				fmt.Println("Invalid file! Enter path again: ")
 			}
-
 		}
 	}
 
